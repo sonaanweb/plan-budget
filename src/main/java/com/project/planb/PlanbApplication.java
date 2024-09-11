62ab8de7989dde69653ef0b47ba7d724b678f189
Sona <objet_an@naver.com>
2024-09-12 00:17:57 +0900

    [CHORE] 초기 세팅

---
+++ src/main/java/com/project/planb/PlanbApplication.java
@@ -0,0 +1,13 @@
+package com.project.planb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/planb/PlanbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.planb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
