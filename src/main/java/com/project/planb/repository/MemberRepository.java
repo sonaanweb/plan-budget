1a10f7f88680cf334b84d22afee88aaafacc1366
Sona <objet_an@naver.com>
2024-09-14 20:40:33 +0900

    [FEAT] 회원가입 req DTO, Member repo 설계

---
+++ src/main/java/com/project/planb/repository/MemberRepository.java
@@ -0,0 +1,15 @@
+package com.project.planb.repository;
+
+import com.project.planb.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member,Long> {
+
+    // account 필드로 Member 엔티티 조회
+    Optional<Member> findByAccount(String account);
+
+    // account 중복 검증
+    boolean existsByAccount(String account);
+}
