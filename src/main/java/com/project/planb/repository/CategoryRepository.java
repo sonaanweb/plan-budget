a9b17381e52449e4ab5db3c7b688150bd8026487
Sona <objet_an@naver.com>
2024-09-18 22:32:23 +0900

    [FEAT] 카테고리 관련 API 기능 구현

--- src/main/java/com/project/planb/repository/CategoryRepository.java
+++ src/main/java/com/project/planb/repository/CategoryRepository.java
@@ -1,2 +1,10 @@
-package com.project.planb.repository;public interface CategoryRepository {
+package com.project.planb.repository;
+
+import com.project.planb.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category,Long> {
+
+    // category 중복 검증
+    boolean existsByCategoryName(String categoryName);
 }
