99b961dd4f2ac79bd31d6ad5a8c74712b35dae8b
Sona <objet_an@naver.com>
2024-09-18 22:31:49 +0900

    [FEAT] 카테고리 예외처리 추가

--- src/main/java/com/project/planb/exception/exceptionType/CategoryExceptionType.java
+++ src/main/java/com/project/planb/exception/exceptionType/CategoryExceptionType.java
@@ -1,2 +1,27 @@
-package com.project.planb.exception.exceptionType;public enum CategoryExceptionType {
+package com.project.planb.exception.exceptionType;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public enum CategoryExceptionType implements ExceptionType {
+
+    DUPLICATED_CATEGORY(HttpStatus.CONFLICT, "이미 존재하는 카테고리입니다."),
+    CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "카테고리를 찾을 수 없습니다.");
+
+    private final HttpStatus status;
+    private final String message;
+
+    @Override
+    public HttpStatus status(){
+        return this.status;
+    }
+
+    @Override
+    public String message(){
+        return this.message;
+    }
 }
