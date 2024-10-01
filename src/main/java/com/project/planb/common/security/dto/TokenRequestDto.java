9aa9e6b240348d0f5c91895789de3fef00381e9e
Sona <objet_an@naver.com>
2024-10-02 03:24:37 +0900

    [CHORE] jwt 변수 설정

---
+++ src/main/java/com/project/planb/common/security/dto/TokenRequestDto.java
@@ -0,0 +1,2 @@
+package com.project.planb.common.security.dto;public record TokenRequestDto() {
+}
---
+++ src/main/java/com/project/planb/common/security/repository/RefreshTokenRepository.java
@@ -0,0 +1,2 @@
+package com.project.planb.common.security.repository;public interface RefreshTokenRepository {
+}
